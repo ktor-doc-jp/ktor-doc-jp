731425a8aaf07bc4453c912206d417accfa5a1b0
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-03-02 10:45:33 +0100

    Add anchor links to headings with id via JavaScript

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-   $(".compact").on("click", function(e) {
-       $(this).toggleClass('expand')
-   });
+    $(".compact").on("click", function(e) {
+        $(this).toggleClass('expand')
+    });
+
+    $(".doc-content").find("h1,h2,h3,h4,h5,h6").filter("[id]").each(function() {
+        var heading = $(this);
+
+        heading.html($('<a />')
+            .addClass('anchored-heading')
+            .attr('href', '#' + heading.attr('id'))
+            .html(heading.html())
+        );
+    });
 });
