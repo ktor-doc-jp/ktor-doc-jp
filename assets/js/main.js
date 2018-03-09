d3b971b9833dba04c122664ab268165def9e0b59
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-03-09 19:30:18 +0100

    Do not affix sidebar for now since it cause problems. Let's revisit it later when a proper layout is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
 	/* Ref: http://getbootstrap.com/javascript/#affix-examples */
 
     	
-	$('#doc-menu').affix({
-        offset: {
-            top: ($('#header').outerHeight(true) + $('#doc-header').outerHeight(true)) + 45,
-            bottom: ($('#footer').outerHeight(true) + $('#promo-block').outerHeight(true)) + 75
-        }
-    });
+	//$('#doc-menu').affix({
+    //    offset: {
+    //        top: ($('#header').outerHeight(true) + $('#doc-header').outerHeight(true)) + 45
+    //        //top: ($('#header').outerHeight(true) + $('#doc-header').outerHeight(true)) + 45,
+    //        //bottom: ($('#footer').outerHeight(true) + $('#promo-block').outerHeight(true)) + 75
+    //    }
+    //});
     
     /* Hack related to: https://github.com/twbs/bootstrap/issues/10236 */
     $(window).on('load resize', function() {
@@ -17,7 +18,7 @@ $(document).ready(function() {
     });
 
     /* Activate scrollspy menu */
-    $('body').scrollspy({target: '#doc-nav', offset: 100});
+    //$('body').scrollspy({target: '#doc-nav', offset: 100});
     
     /* Smooth scrolling */
 	$('a.scrollto').on('click', function(e){
