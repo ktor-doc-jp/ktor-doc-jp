688dade07c5f4b4d740e1388e28c0fab19b35aef
soywiz <soywiz@gmail.com>
2018-09-07 14:04:52 +0200

    Update checkLinks to easily reproduce problem

--- checkLinks.kt
+++ checkLinks.kt
@@ -20,12 +20,14 @@
 @file:DependsOn("io.ktor:ktor-server-netty:0.9.4")
 @file:DependsOn("io.ktor:ktor-client-core:0.9.4")
 @file:DependsOn("io.ktor:ktor-client-apache:0.9.4")
+@file:DependsOn("io.ktor:ktor-client-okhttp:0.9.4")
 @file:DependsOn("io.ktor:ktor-websockets:0.9.4")
 
 @file:EntryPoint("CheckLinks")
 
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
+import io.ktor.client.engine.okhttp.*
 import io.ktor.client.request.*
 import io.ktor.client.response.*
 import kotlinx.coroutines.experimental.*
@@ -50,7 +52,7 @@ object CheckLinks {
 
 	data class Task(val base: String, val full: String)
 
-	val client by lazy { HttpClient(Apache) }
+	//val client by lazy { HttpClient(Apache) }
 	val ids = hashMapOf<String, Set<String>>()
 	val visited = hashSetOf<String>()
 	val queue = java.util.LinkedList<Task>()
@@ -70,6 +72,8 @@ object CheckLinks {
 		}
 	}
 
+	val client = HttpClient(OkHttp)
+
 	suspend fun check(task: Task) {
 		//println("Checking ${task.full}...")
 		val full = task.full
@@ -81,13 +85,17 @@ object CheckLinks {
 		}
 
 		if (url !in ids) {
+			println("url: $url")
 			val response = client.get<HttpResponse>(url)
 			val html = try {
-				response.readText()
+				//response.readBytes().toString(Charsets.UTF_8)
+				//response.readText()
+				response.readText(Charsets.UTF_8)
 			} catch (e: kotlinx.io.charsets.MalformedInputException) {
 				println("[warn] Binary: $url")
 				"" // A Binary or malformed file?
 			}
+			println("done")
 			//println("Downloaded: $url")
 			//if (response.status.isOk()) {
 
