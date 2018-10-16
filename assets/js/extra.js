8577c6603e24e9e4c50ff78a122e5c397507e54a
soywiz <soywiz@gmail.com>
2018-10-16 05:37:34 +0200

    Highlight search results

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -9,8 +9,207 @@ $(document).ready(function() {
             document.getSelection().empty();
             compact.text('+');
         }
-
     }
+
+    /*
+    function getLeafDescendants(node, out = []) {
+        const children = node.childNodes;
+        if (children.length === 0) {
+            out.push(node);
+        } else {
+            for (let n = 0; n < children.length; n++) {
+                const child = children[n];
+                getLeafDescendants(child, out);
+            }
+        }
+        return out
+    }
+
+    function indexesOf(string, pattern) {
+        var match,
+            indexes = {};
+
+        const regex2 = new RegExp(pattern, "g");
+
+        while (match = regex2.exec(string)) {
+            if (!indexes[match[0]]) indexes[match[0]] = [];
+            indexes[match[0]].push(match.index);
+        }
+
+        return indexes;
+    }
+
+    function highlight(highlightText, node) {
+        const children = getLeafDescendants(node);
+        console.log('highlight', node);
+        for (const v of children) {
+            console.log(v.name, v.tagName, v.nodeValue, v);
+
+            //const isLeaf = $v.children().length === 0;
+            //if (isLeaf) {
+            //    const text = $v.text();
+            //    //console.log(v.nodeName);
+            //    const indexes = indexesOf(text, highlightText);
+            //    console.log(text, indexes);
+            //} else {
+            //    const text = $v.text();
+            //    console.log('NO LEAF', text);
+            //}
+
+        }
+    }
+    */
+
+    //function highlight(text, inputText) {
+    //    var innerHTML = inputText.innerHTML;
+    //    var index = innerHTML.indexOf(text);
+    //    if (index >= 0) {
+    //        innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+    //        inputText.innerHTML = innerHTML;
+    //    }
+    //}
+
+    var InstantSearch = {
+
+        "highlight": function (container, highlightText)
+        {
+            var internalHighlighter = function (options)
+                {
+
+                    var id = {
+                            container: "container",
+                            tokens: "tokens",
+                            all: "all",
+                            token: "token",
+                            className: "className",
+                            sensitiveSearch: "sensitiveSearch"
+                        },
+                        tokens = options[id.tokens],
+                        allClassName = options[id.all][id.className],
+                        allSensitiveSearch = options[id.all][id.sensitiveSearch];
+
+
+                    function checkAndReplace(node, tokenArr, classNameAll, sensitiveSearchAll)
+                    {
+                        var nodeVal = node.nodeValue, parentNode = node.parentNode,
+                            i, j, curToken, myToken, myClassName, mySensitiveSearch,
+                            finalClassName, finalSensitiveSearch,
+                            foundIndex, begin, matched, end,
+                            textNode, span, isFirst;
+
+                        for (i = 0, j = tokenArr.length; i < j; i++)
+                        {
+                            curToken = tokenArr[i];
+                            myToken = curToken[id.token];
+                            myClassName = curToken[id.className];
+                            mySensitiveSearch = curToken[id.sensitiveSearch];
+
+                            finalClassName = (classNameAll ? myClassName + " " + classNameAll : myClassName);
+
+                            finalSensitiveSearch = (typeof sensitiveSearchAll !== "undefined" ? sensitiveSearchAll : mySensitiveSearch);
+
+                            isFirst = true;
+                            while (true)
+                            {
+                                if (finalSensitiveSearch)
+                                    foundIndex = nodeVal.indexOf(myToken);
+                                else
+                                    foundIndex = nodeVal.toLowerCase().indexOf(myToken.toLowerCase());
+
+                                if (foundIndex < 0)
+                                {
+                                    if (isFirst)
+                                        break;
+
+                                    if (nodeVal)
+                                    {
+                                        textNode = document.createTextNode(nodeVal);
+                                        parentNode.insertBefore(textNode, node);
+                                    } // End if (nodeVal)
+
+                                    parentNode.removeChild(node);
+                                    break;
+                                } // End if (foundIndex < 0)
+
+                                isFirst = false;
+
+
+                                begin = nodeVal.substring(0, foundIndex);
+                                matched = nodeVal.substr(foundIndex, myToken.length);
+
+                                if (begin)
+                                {
+                                    textNode = document.createTextNode(begin);
+                                    parentNode.insertBefore(textNode, node);
+                                } // End if (begin)
+
+                                span = document.createElement("span");
+                                span.className += finalClassName;
+                                span.appendChild(document.createTextNode(matched));
+                                parentNode.insertBefore(span, node);
+
+                                nodeVal = nodeVal.substring(foundIndex + myToken.length);
+                            } // Whend
+
+                        } // Next i
+                    }; // End Function checkAndReplace
+
+                    function iterator(p)
+                    {
+                        if (p === null) return;
+
+                        var children = Array.prototype.slice.call(p.childNodes), i, cur;
+
+                        if (children.length)
+                        {
+                            for (i = 0; i < children.length; i++)
+                            {
+                                cur = children[i];
+                                if (cur.nodeType === 3)
+                                {
+                                    checkAndReplace(cur, tokens, allClassName, allSensitiveSearch);
+                                }
+                                else if (cur.nodeType === 1)
+                                {
+                                    iterator(cur);
+                                }
+                            }
+                        }
+                    }; // End Function iterator
+
+                    iterator(options[id.container]);
+                } // End Function highlighter
+            ;
+
+
+            internalHighlighter(
+                {
+                    container: container
+                    , all:
+                        {
+                            className: "highlighter"
+                        }
+                    , tokens: [
+                        {
+                            token: highlightText
+                            , className: "highlight"
+                            , sensitiveSearch: false
+                        }
+                    ]
+                }
+            ); // End Call internalHighlighter
+
+        } // End Function highlight
+
+    };
+
+    function highlight(highlightText, node) {
+        if (highlightText !== "") {
+            InstantSearch.highlight(node, highlightText);
+        }
+    }
+
+    //highlight('Ktor', document.body);
 
     $(".compact").each((index, element) => {
         //$(element).prepend(
@@ -235,6 +434,10 @@ $(document).ready(function() {
             outLines.push('<li>No results found</li>');
         }
         searchResults.html(outLines.join(''));
+        const squery = query.replace('#', '');
+        if (squery !== "") {
+            highlight(squery.replace('#', ''), searchResults[0]);
+        }
     }
 
     $('#search-input').on('focus', function() {
